75f5e2d4633139c84304d58d797df0241f420d9e
Mike Conner <connerms@grinnell.edu>
2015-06-30 11:46:35 -0500

    initial commit to push all files

---
+++ common/footer.php
@@ -0,0 +1,31 @@
+</div><!-- end content -->
+
+<footer>
+
+    <div id="footer-content" class="center-div">
+        <?php if($footerText = get_theme_option('Footer Text')): ?>
+        <div id="custom-footer-text">
+            <p><?php echo get_theme_option('Footer Text'); ?></p>
+        </div>
+        <?php endif; ?>
+        <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
+        <p><?php echo $copyright; ?></p>
+        <?php endif; ?>
+        <p class="credits"><?php echo __('<a href="http://omeka.org/add-ons/themes/avantgarde/">Theme adapted from Avantgarde</a> | <a href="http://omeka.org">Powered by Omeka</a>'); ?></p>
+
+    </div><!-- end footer-content -->
+
+     <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
+
+</footer>
+
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        Omeka.showAdvancedForm();
+               Omeka.dropDown();
+    });
+</script>
+
+</body>
+
+</html>
